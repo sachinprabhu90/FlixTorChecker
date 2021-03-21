04f315234a4f1d6f756c36964267cb5ca13e86aa
Sachin Prabhu <sachinprabhu@Sachins-MacBook-Air.local>
2021-03-21 20:01:26 +0530

    minor changes

--- flixtor.py
+++ flixtor.py
@@ -2,25 +2,19 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 import time
-# import pync
 from notifypy import Notify
 
-
+# headless flag
 chrome_options = Options()
-# Chrome v75 and lower:
-# chrome_options.add_argument("--headless")
-# Chrome v 76 and above (v76 released July 30th 2019):
 chrome_options.headless = True
 
-driver = webdriver.Chrome('./chromedriver', chrome_options=chrome_options)
+driver = webdriver.Chrome('./chromedriver', options=chrome_options)
 
 driver.get("https://flixtor.to/")
 time.sleep(5)
 
-# print(driver.find_element_by_xpath('/html/body/div[3]/div[1]/span').text)
 
 while True:
-    print(driver.find_element_by_xpath('/html/body/div[3]/div[1]/span').text)
     if driver.find_element_by_xpath('/html/body/div[3]/div[1]/span').text == 'Welcome to Flixtor':
         notification = Notify()
         notification.title = "FlixTor"
